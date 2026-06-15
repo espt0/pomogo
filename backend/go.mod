ef160346a7917b97c02d42d7efc4ba4b521de7d2
Victor Hugo <victorhugoflorencioss@gmail.com>
2026-06-15 20:58:53 -0300

    feat: Adiciona o model user

---
+++ backend/go.mod
@@ -0,0 +1,12 @@
+module github.com/espt0/pomogo
+
+go 1.25.0
+
+require (
+	github.com/gofrs/uuid/v5 v5.4.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.10.0 // indirect
+	github.com/labstack/echo/v5 v5.2.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
+)
---
+++ backend/internal/model/user.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid/v5"
+)
+
+type User struct {
+	ID           uuid.UUID `json:"id"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type CreateUserRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
