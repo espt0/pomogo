80bb4ee7811080f4a31f05d47daeeadf28f9bed4
Victor Hugo <victorhugoflorencioss@gmail.com>
2026-07-07 21:39:30 -0300

    fix: Adiciona limite no password

--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -18,11 +18,11 @@ type User struct {
 type CreateUserRequest struct {
 	Name     string `json:"name" validate:"required,min=3,max=80"`
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8"`
+	Password string `json:"password" validate:"required,min=8,max=70"`
 }
 
 type UpdateUserRequest struct {
 	Name     *string `json:"name" validate:"omitempty,min=3,max=80"`
 	Email    *string `json:"email" validate:"omitempty,email"`
-	Password *string `json:"password" validate:"omitempty,min=8"`
+	Password *string `json:"password" validate:"omitempty,min=8,max=70"`
 }
