5cbbed834efa3ebff562c7057d34167150e64592
Victor Hugo <victorhugoflorencioss@gmail.com>
2026-07-21 22:19:48 -0300

    feat: adiciona ErrNotFound

--- backend/internal/apperrors/apperrors.go
+++ backend/internal/apperrors/apperrors.go
@@ -7,5 +7,5 @@ import (
 var ErrEmailAlreadyExists = errors.New("email já cadastrado")
 var ErrInvalidEmailOrPassword = errors.New("email ou senha inválidos")
 var ErrUserNotFound = errors.New("usuário não encontrado")
-
+var ErrNotFound = errors.New("recurso não encontrado")
 var ErrInvalidRefreshToken = errors.New("refresh token inválido ou expirado")
